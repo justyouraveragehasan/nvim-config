eb47755254cb3dbe0fad3bfc06ce14ef405231f1
justyouraveragehasan <hasanali.soda@gmail.com>
2024-06-13 14:38:13 +0100

    added completions

--- config/plugins.nix
+++ config/plugins.nix
@@ -49,12 +49,44 @@ plugins = {
 	clangd.enable = true;
 	
 	#rust 
-	rust-analyzer.enable = true;
+	#rust-analyzer.enable = true;
 	
 	#need to add embedded depending on how it all works
 
     };
   };
+
+  cmp-emoji.enable = true;
+  cmp = {
+    enable = true;
+    settings = {
+    autoEnableSources = true;
+    formatting.fields = ["kind" "abbr" "menu"];
+    sources = [
+    {name = "nvim_lsp";}
+    {name = "emoji";}
+    {
+    name = "buffer";
+    option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+    keywordLength = 3;
+    }
+    {
+    name = "path";
+    keywordLength = 3;
+    }
+
+    ];
+    mapping = {
+    "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+    "<C-j>" = "cmp.mapping.select_next_item()";
+    "<C-k>" = "cmp.mapping.select_prev_item()";
+    "<C-e>" = "cmp.mapping.abort()";
+    };
+  };
+  };
+  cmp-nvim-lsp.enable = true;
+  cmp-buffer.enable = true;
+  cmp-path.enable = true;
 };
 }
 
